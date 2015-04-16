56559c78467ef68ee02790b6b4ff9b8a2c0565a5
Sarah Paris <sarah.r.paris@gmail.com>
2015-04-15 22:29:57 -0400

    observer-main: integrated mpu925 but no promises

--- contiki/apps/observer-main/observer-main.c
+++ contiki/apps/observer-main/observer-main.c
@@ -2,9 +2,12 @@
 #include <stdio.h>
 #include "sys/etimer.h"
 #include "dev/leds.h"
+#include "cpu/cc2538/spi-arch.h"
+#include "core/dev/spi.h"
 #include "cpu/cc2538/dev/gpio.h"
 #include "cpu/cc2538/dev/i2c.h"
 #include "si1147.h"
+#include "mpu9250.h"
 
 static struct etimer wait_timer;
 
@@ -16,16 +19,19 @@ AUTOSTART_PROCESSES(&observer_main_process);
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(observer_main_process, ev, data) {
   PROCESS_BEGIN();
-  
+	 
+  int16_t accel_cury; 
   si1147_als_data als_data;
 
   etimer_set(&wait_timer, POLL_PERIOD);
 
+	spi_set_mode(SSI_CR0_FRF_MOTOROLA, SSI_CR0_SPO, SSI_CR0_SPH, 8);
   i2c_init(GPIO_C_NUM, 5, // SDA
            GPIO_C_NUM, 4, // SCL
            I2C_SCL_FAST_BUS_SPEED);   
   
-  si1147_init(0, SI1147_ALS_ENABLE); 
+  si1147_init(SI1147_FORCED_CONVERSION, SI1147_ALS_ENABLE);
+  mpu9250_init(); 
 
   while(1) {
     PROCESS_YIELD();
@@ -33,7 +39,9 @@ PROCESS_THREAD(observer_main_process, ev, data) {
 
     printf("---------------\n");
     i2c_buffer_flush();
+
     si1147_als_force_read(&als_data);
+    accel_cury = mpu9250_readSensor(MPU9250_ACCEL_YOUT_L, MPU9250_ACCEL_YOUT_H);
  
     etimer_reset(&wait_timer);
   }
