b53a31c135b6a9a2e753a26f62d7facdff4e034e
Sarah Paris <sarah.r.paris@gmail.com>
2015-04-17 03:27:23 -0400

    adc121c021: successive write/reads not working

---
+++ contiki/apps/adc121c021/adc121c021.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+
+#include "contiki.h"
+#include "sys/timer.h"
+#include "dev/leds.h"
+#include "cpu/cc2538/dev/i2c.h"
+#include "dev/soc-adc.h"
+#include "adc121c021.h"
+
+static struct timer periodic_timer_blue;
+static struct timer periodic_timer_samples;
+static struct timer adc121c021_sampling_timer;
+
+#define NUM_SAMPLES 2000
+
+/*---------------------------------------------------------------------------*/
+PROCESS(adc121c021_process, "ADC121C021");
+AUTOSTART_PROCESSES(&adc121c021_process);
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(adc121c021_process, ev, data) {
+
+	PROCESS_BEGIN(); 
+
+	timer_set(&periodic_timer_blue, 0.5*CLOCK_SECOND);
+	timer_set(&periodic_timer_samples, 0.05*CLOCK_SECOND);
+  
+  i2c_init(GPIO_C_NUM, 5, // SDA
+           GPIO_C_NUM, 4, // SCL
+           I2C_SCL_NORMAL_BUS_SPEED);
+
+	while(1) {
+    printf("Hello world!\n"); 
+
+    uint16_t amp = adc121c021_read_amplitude();
+    printf("Microphone amplitude: %x\n", amp);
+	  while (!timer_expired(&periodic_timer_blue));
+  	leds_toggle(LEDS_BLUE);
+		timer_restart(&periodic_timer_blue);
+	}
+
+	PROCESS_END();
+}
+
+uint16_t adc121c021_read_amplitude() {
+  uint16_t low, high;
+/*
+  adc121c021_write_reg16(ADC121C021_LOW_CONV, ADC121C021_LOW_CONV_CLEAR);
+  low = adc121c021_read_reg16(ADC121C021_LOW_CONV);
+  adc121c021_write_reg16(ADC121C021_HIGH_CONV, ADC121C021_HIGH_CONV_CLEAR);
+  high = adc121c021_read_reg16(ADC121C021_HIGH_CONV);*/
+
+  // start automatic conversion
+  adc121c021_write_reg8(ADC121C021_CONFIG, 0x20);
+  adc121c021_read_reg8(ADC121C021_CONFIG);
+  
+/*	
+  timer_set(&adc121c021_sampling_timer, ADC121C021_SAMPLING_TIME);
+  while(!timer_expired(&adc121c021_sampling_timer));
+
+  // stop automatic conversion
+  adc121c021_write_reg8(ADC121C021_CONFIG, 0x00);
+
+  low = adc121c021_read_reg16(ADC121C021_LOW_CONV);
+  high = adc121c021_read_reg16(ADC121C021_HIGH_CONV);;
+*/
+  return high-low;
+}
+
+void adc121c021_write_reg8(uint8_t reg_addr, uint8_t data) {
+  uint8_t tx[] = {reg_addr, data};
+
+  i2c_buffer_flush();
+  if (ADC121C021_DBG)
+    printf("adc121c021_write_reg: [0x%x] <- 0x%x\n", reg_addr, data);
+
+  i2c_burst_send(ADC121C021_DEFAULT_SLAVE_ADDR, tx, sizeof(tx));
+  return;
+}
+
+void adc121c021_write_reg16(uint8_t reg_addr, uint16_t data) {
+  uint8_t tx[] = {reg_addr, (uint8_t) (data >> 8), (uint8_t) data};
+
+  i2c_buffer_flush();
+  if (ADC121C021_DBG)
+    printf("adc121c021_write_reg: [0x%x] <- 0x%x\n", reg_addr, data);
+
+  i2c_burst_send(ADC121C021_DEFAULT_SLAVE_ADDR, tx, sizeof(tx));
+  return;
+}
+
+uint8_t adc121c021_read_reg8(uint8_t reg_addr) {
+  uint8_t tx[] = {reg_addr};
+  uint8_t rx[1];
+  i2c_buffer_flush();
+
+  i2c_single_send(ADC121C021_DEFAULT_SLAVE_ADDR, *tx);
+  i2c_single_receive(ADC121C021_DEFAULT_SLAVE_ADDR, rx);
+
+  if (ADC121C021_DBG)
+    printf("adc121c021_read_reg: [0x%x] -> 0x%x\n", reg_addr, *rx);
+  return *rx;
+}
+
+uint16_t adc121c021_read_reg16(uint16_t reg_addr) {
+  uint8_t tx[] = {reg_addr};
+  uint8_t rx[2];
+  uint16_t data;
+  i2c_buffer_flush();
+
+  i2c_single_send(ADC121C021_DEFAULT_SLAVE_ADDR, *tx);
+  i2c_burst_receive(ADC121C021_DEFAULT_SLAVE_ADDR, rx, 2);
+
+  data = rx[0];
+  data = data << 8;
+  data |= rx[1];
+
+  if (ADC121C021_DBG)
+    printf("adc121c021_read_reg: [0x%x] -> 0x%x\n", reg_addr, data);
+  return data;
+}
+
+void i2c_buffer_flush() {
+  int i;
+  for (i=0; i < 9; i++) {
+    i2c_master_command(I2C_MASTER_CMD_BURST_SEND_FINISH);
+    while(i2c_master_busy());
+  }
+}
+
---
+++ contiki/apps/adc121c021/adc121c021.h
@@ -0,0 +1,44 @@
+#ifndef ADC121C021_H
+#define ADC121C021_H
+
+#include "sys/etimer.h"
+
+// when set, enables print statements
+#define ADC121C021_DBG 1
+#define ADC121C021_DBG_ALS 1
+
+#define ADC121C021_STARTUP_TIME 0.025*CLOCK_SECOND
+#define ADC121C021_SAMPLING_TIME 0.05*CLOCK_SECOND
+#define ADC121C021_DEFAULT_SLAVE_ADDR 0x54
+
+// I2C Registers
+#define ADC121C021_CONV_RESULT    0x00 // the only read-only reg
+#define ADC121C021_ALERT_STATUS   0x01
+#define ADC121C021_CONFIG         0x02
+#define ADC121C021_LOW_LIMIT      0x03
+#define ADC121C021_HIGH_LIMIT     0x04
+#define ADC121C021_HYSTERESIS     0x05
+#define ADC121C021_LOW_CONV       0x06
+#define ADC121C021_HIGH_CONV      0x07
+
+#define ADC121C021_LOW_CONV_CLEAR   0x0FFF
+#define ADC121C021_HIGH_CONV_CLEAR  0x0000
+
+typedef union adc121c021_uint16
+{
+  uint16_t val;
+  uint8_t bytes[2];
+} adc121c021_uint16_t;
+
+void i2c_buffer_flush();
+
+void adc121c021_write_reg8(uint8_t reg_addr, uint8_t data);
+void adc121c021_write_reg16(uint8_t reg_addr, uint16_t data);
+
+uint8_t adc121c021_read_reg8(uint8_t reg_addr);
+uint16_t adc121c021_read_reg16(uint16_t reg_addr);
+
+uint16_t adc121c021_read_amplitude();
+
+#endif /*ADC121C021_H*/
+
