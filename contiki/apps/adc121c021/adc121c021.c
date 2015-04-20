1b62c68d25438df4dbefa404a38c4cac4c586270
Sarah Paris <sarah.r.paris@gmail.com>
2015-04-19 18:46:25 -0700

    Cynthia's adc code that was on yorkpatty

--- contiki/apps/adc121c021/adc121c021.c
+++ contiki/apps/adc121c021/adc121c021.c
@@ -28,13 +28,29 @@ PROCESS_THREAD(adc121c021_process, ev, data) {
            GPIO_C_NUM, 4, // SCL
            I2C_SCL_NORMAL_BUS_SPEED);
 
-  adc121c021_write_reg8(ADC121C021_CONFIG, 0x20);
+  adc121c021_write_reg8(ADC121C021_CONFIG, 0x00);
+  adc121c021_read_reg16(ADC121C021_LOW_CONV);
+  adc121c021_read_reg16(ADC121C021_HIGH_CONV);
+  adc121c021_write_reg16(ADC121C021_LOW_CONV, 0x000F);
+  adc121c021_read_reg16(ADC121C021_LOW_CONV);
+  adc121c021_write_reg16(ADC121C021_HIGH_CONV, 0xC12);
+  adc121c021_read_reg16(ADC121C021_HIGH_CONV);
+  adc121c021_write_reg8(ADC121C021_CONFIG, 0xe0);
+  adc121c021_read_reg8(ADC121C021_CONFIG);
+  adc121c021_read_reg16(ADC121C021_HIGH_CONV);
+  while(1) {
+    printf("Hello world!\n");
+    adc121c021_read_reg16(ADC121C021_CONV_RESULT);
+    adc121c021_read_reg8(ADC121C021_LOW_CONV);
+    //adc121c021_write_reg8(ADC121C021_CONFIG, 0xe0);
+    //adc121c021_read_reg8(ADC121C021_CONFIG);
+    ////adc121c021_write_reg8(ADC121C021_CONFIG, 0x40);
+    ////adc121c021_read_reg8(ADC121C021_CONFIG);
+    ////adc121c021_write_reg16(ADC121C021_LOW_LIMIT, 0x0FFF);
+    ////adc121c021_read_reg16(ADC121C021_LOW_LIMIT);
+    //adc121c021_read_reg16(ADC121C021_CONV_RESULT);
 
-
-	while(1) {
-    printf("Hello world!\n"); 
-
-    uint16_t amp = adc121c021_read_amplitude();
+    uint16_t amp =   adc121c021_read_amplitude();
     printf("Microphone amplitude: %x\n", amp);
 	  while (!timer_expired(&periodic_timer_blue));
   	leds_toggle(LEDS_BLUE);
@@ -53,9 +69,10 @@ uint16_t adc121c021_read_amplitude() {
   high = adc121c021_read_reg16(ADC121C021_HIGH_CONV);*/
 
   // start automatic conversion
+  adc121c021_write_reg8(ADC121C021_CONFIG, 0x20);
   adc121c021_read_reg8(ADC121C021_CONFIG);
   
-/*	
+	
   timer_set(&adc121c021_sampling_timer, ADC121C021_SAMPLING_TIME);
   while(!timer_expired(&adc121c021_sampling_timer));
 
@@ -64,7 +81,7 @@ uint16_t adc121c021_read_amplitude() {
 
   low = adc121c021_read_reg16(ADC121C021_LOW_CONV);
   high = adc121c021_read_reg16(ADC121C021_HIGH_CONV);;
-*/
+
   return high-low;
 }
 
@@ -96,12 +113,16 @@ uint8_t adc121c021_read_reg8(uint8_t reg_addr) {
   i2c_buffer_flush();
 
   i2c_single_send(ADC121C021_DEFAULT_SLAVE_ADDR, *tx);
+  //uint8_t tx2[] = {reg_addr, ADC121C021_ALERT_STATUS};
+  //i2c_burst_send(ADC121C021_DEFAULT_SLAVE_ADDR, tx2, 2);
   i2c_single_receive(ADC121C021_DEFAULT_SLAVE_ADDR, rx);
 
   if (ADC121C021_DBG)
     printf("adc121c021_read_reg: [0x%x] -> 0x%x\n", reg_addr, *rx);
   return *rx;
 }
+
+
 
 uint16_t adc121c021_read_reg16(uint16_t reg_addr) {
   uint8_t tx[] = {reg_addr};
@@ -110,6 +131,7 @@ uint16_t adc121c021_read_reg16(uint16_t reg_addr) {
   i2c_buffer_flush();
 
   i2c_single_send(ADC121C021_DEFAULT_SLAVE_ADDR, *tx);
+  i2c_single_receive(ADC121C021_DEFAULT_SLAVE_ADDR, rx);
   i2c_burst_receive(ADC121C021_DEFAULT_SLAVE_ADDR, rx, 2);
 
   data = rx[0];
@@ -117,7 +139,7 @@ uint16_t adc121c021_read_reg16(uint16_t reg_addr) {
   data |= rx[1];
 
   if (ADC121C021_DBG)
-    printf("adc121c021_read_reg: [0x%x] -> 0x%x\n", reg_addr, data);
+    printf("adc121c021_read_reg: [0x%x] -> 0x%x\n", reg_addr, data & 0x0FFF);
   return data;
 }
 
