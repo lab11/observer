b4bd285b275da10dc8ff878dcc1250d24314209c
Alan Zhen <azhen@umich.edu>
2016-04-20 00:22:55 -0400

    changed spi clock divider

--- contiki/apps/triggered_spi_send/project-conf.h
+++ contiki/apps/triggered_spi_send/project-conf.h
@@ -10,6 +10,9 @@
 #define UART_CONF_ENABLE 1
 #define SPI_CONF_DEFAULT_INSTANCE 0
 
+#define SPI0_CONF_CPRS_CPSDVSR 8 // divide clock by 8 so 16MHz/8MHz = 2MHz spi clock
+#define SPI1_CONF_CPRS_CPSDVSR 8
+
 #endif /* PROJECT_CONF_H_ */
 
 /** @} */
